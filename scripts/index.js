45bce41d4de0ff28ac4c4c3e890431ca91255898
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-04-10 22:00:05 -0300

    att.

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,7 @@
-// const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector(".navbar");
+const mobileNavbar = document.querySelector(".navbar__mobile");
+const button = document.querySelector(".burger");
 
-// const mobileNavbar = document.querySelector('.navbar__mobile');
-// const button = document.querySelector('.burger');
-
-
-// button.addEventListener('click', function() {
-
-//     mobileNavbar.classList.toggle('active');
-
-// });
-
-
-// window.addEventListener('scroll', function() {
-//     if (this.window.location.href > 0) return navbar.classList.add('active');
-//     return navbar.classList.remove('active');
-//   });
-
-
-const navbar = document.querySelector('.navbar');
-const mobileNavbar = document.querySelector('.navbar__mobile');
-const button = document.querySelector('.burger');
-
-button.addEventListener('click', function() {
-    mobileNavbar.classList.toggle('active');
+button.addEventListener("click", function () {
+  mobileNavbar.classList.toggle("active");
 });
-
-
-
-
- 
-
-
