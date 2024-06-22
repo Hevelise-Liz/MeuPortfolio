6a1859731cbadc4fc1239116ec5aec00bde00fcb
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-06-21 23:16:46 -0300

    refactor: alteração no modo de exibição dos projetos (de lista para carrossel)

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,49 @@ button.addEventListener("click", function () {
 });
 
 
+
+
+
+
+
+
+
+
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+
+const links = document.querySelectorAll('.navbar-links li a');
+
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+let header = document.getElementById('header');
+
+function createSwiper(container, pagination, nextButton, prevButton) {
+  return new Swiper(container, {
+    slidesPerView: handleWidth(),
+    spaceBetween: 30,
+    pagination: {
+      el: pagination,
+      clickable: true,
+    },
+    navigation: {
+      nextEl: nextButton,
+      prevEl: prevButton,
+    },
+  });
+}
+
+function handleWidth() {
+  let getWidth = window.innerWidth || document.documentElement.clientWidth;
+  let slideShow = 3;
+
+  if (getWidth < 1001) {
+    slideShow = 2;
+  }
+
+  if (getWidth < 700) {
+    slideShow = 1;
+  }
+
+  return slideShow
+}
+
