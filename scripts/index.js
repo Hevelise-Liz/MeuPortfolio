048d2eb7170e8310d8a8c2e14a1d47a03ce5c631
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-07-31 14:16:25 -0300

    refactor: melhorias gerais.

--- scripts/index.js
+++ scripts/index.js
@@ -6,22 +6,17 @@ button.addEventListener("click", function () {
   mobileNavbar.classList.toggle("active");
 });
 
+const menuHamburguer = document.querySelector(".menu-hamburguer");
 
+const links = document.querySelectorAll(".navbar-links li a");
 
-
-
-
-
-
-
-
-
-const menuHamburguer = document.querySelector('.menu-hamburguer');
-
-const links = document.querySelectorAll('.navbar-links li a');
-
-let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
-let header = document.getElementById('header');
+let swiper = createSwiper(
+  ".mySwiper",
+  ".swiper-pagination",
+  ".swiper-button-next",
+  ".swiper-button-prev"
+);
+let header = document.getElementById("header");
 
 function createSwiper(container, pagination, nextButton, prevButton) {
   return new Swiper(container, {
@@ -50,6 +45,5 @@ function handleWidth() {
     slideShow = 1;
   }
 
-  return slideShow
+  return slideShow;
 }
-
